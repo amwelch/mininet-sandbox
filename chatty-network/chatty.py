cbdc648d6e030b752f37a746f85431c53ec34bbc
Sandy <amwelch@umich.edu>
2015-05-16 02:27:33 +0000

    spin up server then run clients in serial

--- chatty-network/chatty.py
+++ chatty-network/chatty.py
@@ -11,8 +11,6 @@ from mininet.node import Node
 from mininet.topolib import TreeTopo
 from mininet.util import waitListening
 
-SERVER_PORT = 8080
-
 def TreeNet(depth=1, fanout=2, **kwargs):
     topo = TreeTopo(depth, fanout)
     return Mininet(topo, **kwargs)
@@ -25,20 +23,40 @@ def connectToRootNS(network, switch, ip, routes):
     for route in routes:
         root.cmd('route add -net {} dev {} '.format(route, intf)) 
 
-def chatserver(network, cmd='webserver.py', hosts=None, routes=None, switch=None):
-    if not switch:
-        switch = network['s1']
-    if not routes:
-        routes = ['10.0.0.0/24']
-    connectToRootNS(network, switch, ip, routes)
-    for host in network.hosts:
-        host.cmd('{} &'.format(cmd))
+def chatserver(host, cwd, hostname, port, cmd='{}webserver.py --addr {} --port {}'):
+    cmd = '{} &'.format(cmd.format(cwd, hostname, port))
+    host.cmd(cmd)
     print "*** Starting webserver ***"
-    for server in hosts:
-        waitListening(server=server, port=SERVER_PORT, timeout=5)
+    print cmd
+#    waitListening(server=hostname, port=port, timeout=5)
+
+def client(host, cwd, hostname, port, cmd='{}client.py --host {} --port {}'):
+    print "**** Running Client ****"
+    host.cmd('{}'.format(cmd.format(cwd, hostname, port)))
 
 def run_cmd(cmd, cwd):
     subprocess.check_call([cmd], shell=True, cwd=cwd)
+
+def getcwd():
+    return os.path.join(os.getcwd(), 'bin/greenlet-chatserver-example/')
+
+def get_ip(host):
+    '''
+    Gets the ip on eth0
+    '''
+    return host.intfs[0].ip
+
+ 
+def run():
+    network = TreeNet(depth=1, fanout=4)
+    webserver,clients = (network.hosts[0], network.hosts[1:])
+    port = 80
+    chatserver(webserver, getcwd(), get_ip(webserver), port)
+    for host in clients:
+        client(host, getcwd(), get_ip(webserver), port)
+    webserver.cmd('pkill -9 -f {}'.format('webserver.py'))
+    network.stop()
+    
 
 def install():
     '''
@@ -66,4 +84,4 @@ def install():
         run_cmd(cmd, cwd = code_bin)
 
 if __name__ == '__main__':
-    install()
+    run()
