9cff7412ad72305510a550af78710197ae37815b
Sandy <amwelch@umich.edu>
2015-06-28 01:05:33 +0000

    some cleanup

--- bgp-sample/network.py
+++ bgp-sample/network.py
@@ -12,6 +12,9 @@ from time import sleep, time
 from multiprocessing import Process
 from argparse import ArgumentParser
 
+import os
+import sys
+
 #Taken/Derived from https://bitbucket.org/jvimal/bgp/overview
 #https://github.com/mininet/mininet/wiki/BGP-Path-Hijacking-Attack-Demo
 
@@ -52,9 +55,6 @@ class SimpleTopo(Topo):
         AS3
     """
 
-    slow_conn = {'bw': 1}
-    fast_conn = {'bw': 10}
- 
     def __init__(self):
         # Add default members to class.
         super(SimpleTopo, self ).__init__()
@@ -65,7 +65,7 @@ class SimpleTopo(Topo):
         routers = []
         for i in xrange(num_ases):
             router = self.addSwitch('R%d' % (i+1))
-	      routers.append(router)
+            routers.append(router)
         hosts = []
         for i in xrange(num_ases):
             router = 'R%d' % (i+1)
@@ -74,17 +74,63 @@ class SimpleTopo(Topo):
                 host = self.addNode(hostname)
                 hosts.append(host)
                 self.addLink(router, host)
-
+  
         #AS2 has a 1 MBPS link while AS3 has 10 MBPS
-        self.addLink('R1', 'R2', **slow_conn)
-        self.addLink('R2', 'R4', **slow_conn)
-        self.addLink('R1', 'R3', **fast_conn)
-        self.addLink('R3', 'R4', **fast_conn)
+        self.addLink('R1', 'R2')
+        self.addLink('R2', 'R4')
+        self.addLink('R1', 'R3')
+        self.addLink('R3', 'R4')
 
-        for j in xrange(num_hosts_per_as):
-            hostname = 'h%d'.format(j)
-            host = self.addNode(hostname)
-            hosts.append(host)
-            self.addLink('R{}'.format(j), hostname)
+        for asn in range(num_ases):
+            for j in range(num_hosts_per_as):
+                hostname = 'h{}-{}'.format(asn, j)
+                host = self.addNode(hostname)
+                hosts.append(host)
+                self.addLink('R{}'.format(asn+1), hostname)
         return
 
+def get_host_id(hostname):
+    return int(hostname.split('-')[0])
+
+def getIP(hostname, asn):
+    num = get_host_id(hostname)
+    AS = int(AS)
+    ip = '%s.0.%s.1/24' % (10+asn, num)
+    return ip
+
+def getGateway(hostname, asn):
+    num = get_host_id(hostname)
+    gw = '%s.0.%s.254' % (10+asn, num)
+    return gw
+
+def main():
+    os.system("rm -f /tmp/R*.log /tmp/R*.pid logs/*")
+    os.system("mn -c >/dev/null 2>&1")
+    os.system("killall -9 bird > /dev/null 2>&1")
+
+    net = Mininet(topo=SimpleTopo(), switch=Router)
+    net.start()
+    for router in net.switches:
+        router.cmd("sysctl -w net.ipv4.ip_forward=1")
+        router.waitOutput()
+
+    sleep_time = 10
+    print "Waiting {} seconds for sysctl changes to take effect...".format(sleep_time)
+    sleep(sleep_time)
+
+    for router in net.switches:
+        router.cmd("bird 2>&1" % (router.name, router.name, router.name), shell=True)
+        router.waitOutput()
+        log("Starting bird on %s" % router.name)
+
+    for host in net.hosts:
+        host.cmd("ifconfig %s-eth0 %s" % (host.name, getIP(host.name)))
+        host.cmd("route add default gw %s" % (getGateway(host.name)))
+
+    CLI(net)
+    net.stop()
+    os.system("killall -9 bird")
+
+
+if __name__ == "__main__":
+    main()
