e2c59adb0525172bc5b8b9007a82ee25a40459a0
Sandy <amwelch@umich.edu>
2015-06-30 01:47:16 +0000

    Add basic bird config templates

--- bgp-sample/network.py
+++ bgp-sample/network.py
@@ -108,6 +108,46 @@ def getGateway(hostname):
     gw = '%s.0.%s.254' % (10+asn, num)
     return gw
 
+bird_template = '''
+log syslog all;
+
+router id {local_ip};
+
+protocol kernel \\{
+   import all;
+   export all;
+\\}
+{neighbors}
+'''
+neighbor_template = '''
+protocol bgp as{remote_as} \\{
+    import all;
+    export all;
+    local as {local_as};
+    source {local_ip};
+    neighbor {neighbor_ip} as {remote_as};
+\\}
+'''
+
+def get_bird_conf(hostname):
+    return '/usr/loca/etc/bird.{}.conf'.format(hostname)
+
+def write_bgp_conf(hostname, neighbors):
+    local_asn = get_router_asn(hostname)
+    local_ip = get_router_ip(hostname)
+
+    neighbor_buf = ''
+    for neighbor in neighbors:
+        asn = get_router_asn(neighbor)
+        ip = get_router_ip(neighbor)
+        neighbor_buf += neighbor_template.format(remote_as=asn, 
+            local_as=local_asn, local_ip=local_ip, neighbor_ip=ip)
+
+    buf = bird_template.format(local_ip=ip, neighbors=neighbor_buf)
+    ofn = get_bird_conf(hostname)
+    with open(ofn, 'w') as fp:
+        fp.write(buf)
+
 def main():
     os.system("rm -f /tmp/R*.log /tmp/R*.pid logs/*")
     os.system("mn -c >/dev/null 2>&1")
@@ -123,8 +163,15 @@ def main():
     print "Waiting {} seconds for sysctl changes to take effect...".format(sleep_time)
     sleep(sleep_time)
 
+    #Write the bird config files
+    write_bgp_conf('R1', ['R2', 'R3'])
+    write_bgp_conf('R2', ['R1', 'R4'])
+    write_bgp_conf('R3' ['R1', 'R4'])
+    write_bgp_conf('R4' ['R2', 'R3'])
+
     for router in net.switches:
-        router.cmd("bird 2>&1", shell=True)
+        conf = get_bird_conf(router.name)
+        router.cmd("bird -c {} 2>&1".format(conf), shell=True)
         router.waitOutput()
         print "Starting bird on %s" % router.name
 
